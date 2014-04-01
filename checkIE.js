1c6f69143f13099851216cc7a16a4addae3bdf38
mleiman <marcin.leiman@gmail.com>
2014-04-01 09:54:48 +0200

    Update checkIE.js

--- checkIE.js
+++ checkIE.js
@@ -1,20 +1,18 @@
 function checkIE(){
 
-	var ie = (navigator.appName == "Microsoft Internet Explorer")? true : false;
-	var appname = navigator.appName;
-	var version = navigator.appVersion;
-	var codename = navigator.appCodeName;
-	var useragent = navigator.userAgent;
-	var rvPos = useragent.indexOf("rv:");
-	var rv = Number(useragent.substring(rvPos+3, rvPos+5));
+var appname = navigator.appName;
+var version = navigator.appVersion;
+var codename = navigator.appCodeName;
+var useragent = navigator.userAgent;
 
-	document.write(ie + "<br />");
-	document.write(appname + "<br />");
-	document.write(version + "<br />");
-	document.write(codename + "<br />");
-	document.write(useragent + "<br />");
-	document.write(rvPos + "<br />");
-	document.write(rv + "<br />");
+
+
+document.write(appname + "<br />");
+document.write(version + "<br />");
+document.write(codename + "<br />");
+document.write(useragent + "<br />");
+
+
 }
 
 function getInternetExplorerVersion()
@@ -25,7 +23,7 @@ function getInternetExplorerVersion()
   if (navigator.appName == 'Microsoft Internet Explorer')
   {
     var ua = navigator.userAgent;
-    var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+    var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
     if (re.exec(ua) != null)
       rv = parseFloat( RegExp.$1 );
   }
@@ -41,9 +39,9 @@ function checkVersion()
     {
 
       document.write("IE " + ver);
-    } 
+    }
 
-    document.write("IE " + ver);	   
+    document.write("IE " + ver);	
   }
 }
 
