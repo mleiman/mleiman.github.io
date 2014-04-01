5526136cb340ab0d50a7efa46ec7974157fb7f81
mleiman <marcin.leiman@gmail.com>
2014-04-01 09:45:13 +0200

    test

---
+++ checkIE.js
@@ -0,0 +1,53 @@
+/*function checkIE(){
+
+	var ie = (navigator.appName == "Microsoft Internet Explorer")? true : false;
+	var appname = navigator.appName;
+	var version = navigator.appVersion;
+	var codename = navigator.appCodeName;
+	var useragent = navigator.userAgent;
+	var rvPos = useragent.indexOf("rv:");
+	var rv = Number(useragent.substring(rvPos+3, rvPos+5));
+
+	document.write(ie + "<br />");
+	document.write(appname + "<br />");
+	document.write(version + "<br />");
+	document.write(codename + "<br />");
+	document.write(useragent + "<br />");
+	document.write(rvPos + "<br />");
+	document.write(rv + "<br />");
+}*/
+
+function getInternetExplorerVersion()
+// Returns the version of Internet Explorer or a -1
+// (indicating the use of another browser).
+{
+  var rv = -1; // Return value assumes failure.
+  if (navigator.appName == 'Microsoft Internet Explorer')
+  {
+    var ua = navigator.userAgent;
+    var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null)
+      rv = parseFloat( RegExp.$1 );
+  }
+  return rv;
+}
+function checkVersion()
+{
+  var ver = getInternetExplorerVersion();
+
+  if ( ver > -1 )
+  {
+    if ( ver < 8.0 )
+    {
+
+      alert("IE " + ver);
+    } 
+
+    
+  }
+}
+
+checkVersion();
+
+
+
