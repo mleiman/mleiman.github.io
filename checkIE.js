dd7af6da5c81305f6f635c9800b02f0b32bcfe46
marcin.leiman <marcin.leiman@best.net.pl>
2014-04-11 14:24:42 +0200

    update

--- checkIE.js
+++ checkIE.js
@@ -17,13 +17,18 @@ function checkBrowser() {
   
     if ( ver > -1 ) {
         if ( ver < 9 ) {
+          alert("ie");
+          document.innerHTML("<h1>IE</h1>");
            
           return 0;
         } else {
-
+          alert("not ie");
+          document.innerHTML("<h1>Not IE</h1>");
           return 1;
         }
     } else {
+        alert("not ie");
+        document.innerHTML("<h1>Not IE</h1>");
         
         return 1;
     }
@@ -33,14 +38,11 @@ function checkBrowser() {
 function(){
 
 if(checkBrowser() == 1) {
-  var info = "<h1>This feature requires Internet Explorer 8 or higher.</h1><h2>Please update your browser.</h2>";
 
   if (checkBrowser()) {
-    alert("not ie");
-    document.innerHTML("<h1>Not IE</h1>");
+    
   } else {
-    alert("ie");
-    document.innerHTML("<h1>IE</h1>");
+    
   }
 
 
