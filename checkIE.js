920aeba8cf2afd7a06f85c4292214ea5ff1f3e70
marcin.leiman <marcin.leiman@best.net.pl>
2014-04-11 14:16:27 +0200

    update

--- checkIE.js
+++ checkIE.js
@@ -1,21 +1,4 @@
-function checkIE() {
-
-var appname = navigator.appName;
-var version = navigator.appVersion;
-var useragent = navigator.userAgent;
-
-
-
-document.write(appname + "<br />");
-document.write(version + "<br />");
-document.write(useragent + "<br />");
-
-
-}
-
-// Returns the version of Internet Explorer or a -1
-// (indicating the use of another browser).
-function getInternetExplorerVersion() {
+function getIEVersion() {
   var rv = -1; // Return value assumes failure.
   if (navigator.appName == 'Microsoft Internet Explorer') {
     var ua = navigator.userAgent;
@@ -23,36 +6,41 @@ function getInternetExplorerVersion() {
     if (re.exec(ua) != null)
       rv = parseFloat( RegExp.$1 );
   }
-  
+
   return rv;
 }
 
 
-function checkVersion() {
-
-  var info = "<h1>This feature requires Internet Explorer 8 or higher.</h1><h2>Please update your browser.</h2>";
+function checkBrowser() {
 
   var ver = getInternetExplorerVersion();
-
-
-  document.write("<br /> IE ver = "+ ver + "<br />");
   
-  if ( ver > -1 ) {
-    
-    if ( ver < 8.0 ) {
+    if ( ver > -1 ) {
+        if ( ver < 9 ) {
+           
+        return 0;
+        } else {
 
-      // content = document.getElementById("content"); 
-      // content.innerHTML = info;
-      // content.style.textAlign = "center";
-
-      document.write("<br />"+ ver + " <  8.0 <br />");
+          return 1;
+        }
+    } else {
+        
+        return 1;
     }
-  
-  }
 }
 
-checkIE();
-checkVersion();
 
+
+if(checkBrowser()) {
+  var info = "<h1>This feature requires Internet Explorer 8 or higher.</h1><h2>Please update your browser.</h2>";
+
+  if (checkBrowser()) {
+    document.innerHTML("<h1>Not IE</h1>");
+  } else {
+    document.innerHTML("<h1>IE</h1>");
+  }
+
+
+}
 
 
